3792d6b063629a91abf4f55ccbe3f8477540470c
zkrowiak@intersystems.com <zkrowiak@intersystems.com>
2019-10-17 12:21:26 -0400

    began adding tab display

--- travel_app/static/frontEnd.js
+++ travel_app/static/frontEnd.js
@@ -10,8 +10,37 @@ function viewModel() {
 
     destinations= {
 
-        getDestinations: function() {
-            alert('gettingDestinations')
+        get: function() {
+            //get data through fetch
+
+            //hide remove all other children to the info-window 
+             tabs = $('.contentArea')
+             for (tab of tabs) {
+                 $(tab).hide()
+             }
+             $('#destinations').show()
+
+            //show destinations window (with creation form)
+
+            //populate data
+        }
+    }
+
+    hotels= {
+
+        get: function() {
+            //get data through fetch
+
+            //hide remove all other children to the info-window 
+             tabs = $('.contentArea')
+             for (tab of tabs) {
+                 $(tab).hide()
+             }
+             $('#hotels').show()
+
+            //show destinations window (with creation form)
+
+            //populate data
         }
     }
 
