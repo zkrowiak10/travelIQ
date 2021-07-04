20a6bdf25ab61ceff0fbe30e97fb81ed89abedc8
zkrowiak10 <zkrowiak10@gmail.com>
2021-07-03 23:39:55 -0400

    fixed app

--- travel_app/config.py
+++ travel_app/config.py
@@ -10,18 +10,16 @@ class Config():
     def SQLALCHEMY_DATABASE_URI(self):
         return "postgresql://{}:{}@{}/".format(self.db_user, self.db_password, self.dbhost) + self.DATABASE
 
-
-
-class Development(Config):
+class Debug(Config):
     SECRET_KEY = 'dev'
+
+class Testing(Debug):
     DEBUG = True
     TESTING = True
+
+class Development(Testing):
     db_user = "postgres"
     db_password = os.environ['POSTGRES_ENV_POSTGRES_PASSWORD']
     DATABASE="postgres"
-    dbhost = os.environ['POSTGRES_PORT_5432_TCP_ADDR']
-    SEND_FILE_MAX_AGE_DEFAULT = 0
+    dbhost = 'postgres:5432'
     # SQLALCHEMY_ECHO = True
-
-class Testing(Development):
-    DATABASE = "mytestdb"
