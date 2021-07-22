b4ee4d6ff10222f69ec4695bb215a22f6fd7a979
zkrowiak10 <zkrowiak10@gmail.com>
2021-07-21 21:11:18 -0400

    annotated/cleaned up backend

--- travel_app/__init__.py
+++ travel_app/__init__.py
@@ -2,16 +2,16 @@
 import os
 from travel_app import config, models, models, urls, ajax
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import logging
 import sys
 
 logging.basicConfig(level=logging.DEBUG, format="%(levelname)s - %(message)s")
-logging.StreamHandler(sys.stdout)
+logging.StreamHandler(sys.stdout) #Send loggint to stout so they are written to heroku logs
 
+# App factory
 def create_app():
-    configEnv = os.getenv('FLASK_ENV')
+    configEnv = os.environ['FLASK_ENV']
     if configEnv == "production":
         configParam = config.Production()
         logging.getLogger().setLevel(logging.INFO)
@@ -19,16 +19,15 @@ def create_app():
         configParam = config.Development()
         logging.getLogger().setLevel(logging.DEBUG)
     else:
-        logging.error("no environment variable specified")
-        raise Exception
+        raise Exception("{configEnv}is not a valid configuration parameter".format(configEnv=configEnv))
     app = Flask(__name__)
     app.config.from_object(configParam)
     app.cli.add_command(models.init_db_command)
     
     models.db.init_app(app)
-    migrate = Migrate(app, models.db)
-    # ensure the instance folder exists
-   
+    migrate = Migrate(app, models.db)   
+
+    # Register routes
     app.register_blueprint(urls.bp)
     app.register_blueprint(urls.iq)
     app.register_blueprint(ajax.ajax)
